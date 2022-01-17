0ff775f1c443cff063f5b8892c9e7f658e3fe59b
docsteampunk <larmore.daniel@gmail.com>
2022-01-17 15:22:11 -0700

    Trying to add change page on form submit
    Needless to say. It's not working well.

--- scripts/settingsScript.js
+++ scripts/settingsScript.js
@@ -1,11 +1,16 @@
-var playerInfo1 = document.getElementById("player1");
-var playerInfo2 = document.getElementById("player2");
-var isBot = document.getElementById("isBot");
-
 function test() {
-    localStorage.setItem("player1", "Player 1");
-    localStorage.setItem("player2", playerInfo2.value);
+    var playerInfo1 = document.getElementById("player1").value;
+    var playerInfo2 = document.getElementById("player2").value;
+    var isBot = document.getElementById("isBot");
 
+    alert("Saving Page... Do not turn off computer");
+
+    if (playerInfo1 == null) {
+        localStorage.setItem("player1", playerInfo1.value);
+    }
+    if (playerInfo2 == null) {
+        localStorage.setItem("player2", playerInfo2.value);
+    }
 
     if (isBot.value == "human") {
         localStorage.setItem("isBot", "person");
@@ -13,7 +18,4 @@ function test() {
     else {
         localStorage.setItem("isBot", "comp");
     }
-
-    alert("AAAH!!!");
-    window.location.href = 'google.com'
 }
