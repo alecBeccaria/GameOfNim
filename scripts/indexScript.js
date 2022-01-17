379d648144ddff4193589ffded2f87b86453f33f
docsteampunk <larmore.daniel@gmail.com>
2022-01-17 14:14:51 -0700

    Form index
    Making data for forms

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,1 +1,8 @@
 console.log('Hello Sir');
+
+const playerInfo1 = document.getElementById("player1");
+const playerInfo2 = document.getElementById("player2");
+
+function select() {
+    alert("HI!");
+}
