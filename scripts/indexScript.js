79345ecec2c3e70d730d77c3558cd8ee4a6de48e
docsteampunk <larmore.daniel@gmail.com>
2022-01-17 14:29:56 -0700

    Added localStorage

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -3,6 +3,18 @@ console.log('Hello Sir');
 const playerInfo1 = document.getElementById("player1");
 const playerInfo2 = document.getElementById("player2");
 
-function select() {
-    alert("HI!");
+function test() {
+    if (playerInfo1.value.trim() === "") {
+        localStorage.setItem("player1", "Player 1");
+    }
+    else {
+        localStorage.setItem("player1", playerInfo1.value);
+    }
+
+    if (playerInfo2.value.trim() === "") {
+        localStorage.setItem("player1", "Player 2");
+    }
+    else {
+        localStorage.setItem("player1", playerInfo2.value);
+    }
 }
