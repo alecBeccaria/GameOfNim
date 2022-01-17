1fd496bba53cca5c435fc07f880b1e83578d11dd
Poke711 <wmejiapalma@student.neumont.edu>
2022-01-17 15:26:01 -0700

    created main menu with basic button functionality
    Buttons are actually divs meme

---
+++ scripts/indexScript.js
@@ -0,0 +1,12 @@
+var newGame = document.getElementById("newGame");
+var rules = document.getElementById("rules");
+
+function button() {
+
+}
+newGame.addEventListener("click", e=>{
+    location.href = "/settings.html";
+})
+rules.addEventListener("click", e=>{
+    location.href = "/rules.html";
+})
