b44e6c47b4f159d0526758682f3231fac1941591
docsteampunk <larmore.daniel@gmail.com>
2022-01-18 09:07:29 -0700

    Made the localStorage work now.

--- scripts/settingsScript.js
+++ scripts/settingsScript.js
@@ -1,16 +1,16 @@
-function test() {
-    var playerInfo1 = document.getElementById("player1").value;
-    var playerInfo2 = document.getElementById("player2").value;
-    var isBot = document.getElementById("isBot");
+var playerInfo1 = document.getElementById("player1");
+var playerInfo2 = document.getElementById("player2");
+var isBot = document.getElementById("isBot");
 
-    alert("Saving Page... Do not turn off computer");
+function test(e) {
+    e.preventDefault();
 
-    if (playerInfo1 == null) {
+    if (playerInfo1 != null) {
         localStorage.setItem("player1", playerInfo1.value);
     }
-    if (playerInfo2 == null) {
+    if (playerInfo2 != null) {
         localStorage.setItem("player2", playerInfo2.value);
-    }
+    } 
 
     if (isBot.value == "human") {
         localStorage.setItem("isBot", "person");
@@ -18,4 +18,7 @@ function test() {
     else {
         localStorage.setItem("isBot", "comp");
     }
+
+    alert("Settings Saved.");
+    window.location.href = "index.html";
 }
