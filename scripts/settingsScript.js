f0f850d3a3a63281db65136bebfa5cc84f4e6caf
docsteampunk <larmore.daniel@gmail.com>
2022-01-18 18:22:40 -0700

    Removed bot data.
    Who needs that anyway?

--- scripts/settingsScript.js
+++ scripts/settingsScript.js
@@ -1,6 +1,5 @@
 var playerInfo1 = document.getElementById("player1");
 var playerInfo2 = document.getElementById("player2");
-var isBot = document.getElementById("isBot");
 
 function test(e) {
     e.preventDefault();
@@ -11,13 +10,6 @@ function test(e) {
     if (playerInfo2 != null) {
         localStorage.setItem("player2", playerInfo2.value);
     } 
-
-    if (isBot.value == "human") {
-        localStorage.setItem("isBot", "person");
-    }
-    else {
-        localStorage.setItem("isBot", "comp");
-    }
 
     alert("Settings Saved.");
     window.location.href = "index.html";
