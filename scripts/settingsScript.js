56d7ea52fe70a7a41eb127900e2773d8f0e2c893
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-01-18 18:31:58 -0700

    Default values for settings page

--- scripts/settingsScript.js
+++ scripts/settingsScript.js
@@ -4,12 +4,16 @@ var playerInfo2 = document.getElementById("player2");
 function test(e) {
     e.preventDefault();
 
-    if (playerInfo1 != null) {
+    if (playerInfo1 != null && playerInfo1.value != "") {
         localStorage.setItem("player1", playerInfo1.value);
+    }else{
+        localStorage.setItem("player1", "Player 1")
     }
-    if (playerInfo2 != null) {
+    if (playerInfo2 != null && playerInfo2.value != "") {
         localStorage.setItem("player2", playerInfo2.value);
-    } 
+    } else{
+        localStorage.setItem("player2", "Player 2")
+    }
 
     alert("Settings Saved.");
     window.location.href = "index.html";
