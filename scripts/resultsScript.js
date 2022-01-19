2f0cb170f679e6ea4cbeccfa4b6ccb90eea70739
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-01-19 16:47:12 -0700

    Added Return to button

--- scripts/resultsScript.js
+++ scripts/resultsScript.js
@@ -3,3 +3,7 @@ const resultsDiv = document.getElementById("results");
 var winNum = localStorage.getItem("winner");
 
 resultsDiv.innerHTML = `Player ${winNum} Wins!`;
+
+const newGame = ( ) => {
+    window.location.href = "./settings.html"
+}
